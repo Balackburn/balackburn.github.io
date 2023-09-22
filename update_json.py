df4bc43a6d422b5e903fd6684dcebd3a05413a9b
Balackburn <actions@github.com>
2023-09-22 12:24:39 +0200

    added more sources details

--- update_json.py
+++ update_json.py
@@ -3,7 +3,7 @@ import requests
 
 def fetch_apps_from_url(url):
     response = requests.get(url)
-    response.raise_for_status() 
+    response.raise_for_status()
     return response.json()["apps"]
 
 def combine_sources(urls):
@@ -13,24 +13,35 @@ def combine_sources(urls):
         combined_apps.extend(apps)
     return combined_apps
 
-def write_to_json_file(apps, name, identifier, filename="apps.json"):
+def write_to_json_file(apps, name, identifier, subtitle, description, iconURL, website, tintColor, filename="apps.json"):
     data = {
-        "name": name, 
-        "identifier": identifier,
+        "name": name,
+        "identifier": identifier, 
+        "subtitle": subtitle,
+        "description": description,
+        "iconURL": iconURL,
+        "website": website,
+        "tintColor": tintColor,
         "apps": apps
     }
     
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
+name = "Balackburn Source"
+identifier = "com.balackburn.source"
+subtitle = "Balackburn's Github Projects"
+description = "This source contains Balackburn's Github Projects"
+iconURL = "https://balackburn.github.io/images/balackburn_icon.png"
+website = "https://balackburn.github.io/"  
+tintColor = "#62c855"
+
 urls = [
     "https://raw.githubusercontent.com/Balackburn/YTLitePlusAltstore/main/apps.json",
     "https://raw.githubusercontent.com/Balackburn/Apollo/main/apps.json",
-    "https://raw.githubusercontent.com/Balackburn/Winston/main/apps.json", 
+    "https://raw.githubusercontent.com/Balackburn/Winston/main/apps.json",
 ]
 
-name = "Balackburn Github Projects"
-identifier = "com.balackburb.source"
-
 combined_apps = combine_sources(urls)
-write_to_json_file(combined_apps, name, identifier)
+
+write_to_json_file(combined_apps, name, identifier, subtitle, description, iconURL, website, tintColor)
